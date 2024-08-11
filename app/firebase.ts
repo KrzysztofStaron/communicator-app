1064f863b3ebe186fc92b279d75ff4980ff50d36
Krzysztof Staroń <kisiel3141@gmail.com>
2024-08-11 21:49:19 +0200

    init

---
+++ app/firebase.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCAV3quMbg_2MLWNllOsBwhzgr7eRZu7TY",
+  authDomain: "nextjs-messanger-ae449.firebaseapp.com",
+  projectId: "nextjs-messanger-ae449",
+  storageBucket: "nextjs-messanger-ae449.appspot.com",
+  messagingSenderId: "731584483024",
+  appId: "1:731584483024:web:5a7e6d96e1986519cc58d5",
+  measurementId: "G-JG2WETM986",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
